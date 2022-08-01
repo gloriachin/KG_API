a8b9c5ff7db27d56c2d23f73d532386de5c7dd69
Katie Christensen <katiechristensen@Katies-MacBook-Pro.local>
2022-07-31 20:08:47 -0700

    updates to the cypher query for gene gene database

--- create_graph_db/gene_gene_cypher.cypher
+++ create_graph_db/gene_gene_cypher.cypher
@@ -1,3 +1,22 @@
-CREATE CONSTRAINT FOR (g:Gene) REQUIRE g.Name IS UNIQUE;
+CREATE CONSTRAINT FOR (s:Gene) REQUIRE s.ID IS UNIQUE;
+CREATE CONSTRAINT FOR (o:Gene) REQUIRE o.ID IS UNIQUE;
 
-USING PERIODIC 
+USING PERIODIC COMMIT
+LOAD CSV WITH HEADERS
+FROM 'file:///gene_gene_formatted.csv' AS row
+
+MERGE (s:Gene {ID: row.'Subject.id'})
+SET s.Symbol = row.'Subject.symbol',
+    s.Prefixes = row.'Subject.id prefixes'
+
+MERGE (o:Gene {ID: row.'object.id'})
+SET o.Symbol = row.'object.symbol',
+    o.Prefixes = row.'object.id prefixes'
+
+MERGE (s)-[p:PHYSICALLY_INTERACTS_WITH]->(o)
+WHERE row.'ASSOCIATION.Predicate' ...?
+
+// or I could do:
+
+MERGE (s)-[p:ASSOCIATION {name: 'PHYSICALLY INTERACTS WITH'}]->(o)
+WHERE ...?
--- create_graph_db/gene_gene_database.py
+++ create_graph_db/gene_gene_database.py
@@ -1,29 +1,26 @@
 from neo4j import GraphDatabase
 
 class ConnectGraphDatabase:
-    def __init__(self, uri, user, pwd):
-        self.__uri = uri
-        self.__user = user
-        self.__pwd = pwd
-        self.__driver = None
+    def __init__(self, uri, user, password):
+        self.driver = None
         try:
-            self.__driver = GraphDatabase.driver(self.__uri, auth=(self.__user, self.__pwd))
+            self.driver = GraphDatabase.driver(uri, auth=(user, password))
         except Exception as e:
             print("Could not create the driver", e)
         
     def close(self):
-        if self.__driver is not None:
-            self.__driver.close()
+        if self.driver is not None:
+            self.driver.close()
         
     def query(self, query, db=None):
-        assert self.__driver is not None, "The driver was not fully initialized"
+        assert self.driver is not None, "The driver was not fully initialized"
         session = None
         response = None
         try: 
-            session = self.__driver.session(database=db) if db is not None else self.__driver.session() 
+            session = self.driver.session(database=db) if db is not None else self.driver.session() 
             response = list(session.run(query))
         except Exception as e:
-            print("The Query did not complete:", e)
+            print("The query did not complete:", e)
         finally: 
             if session is not None:
                 session.close()
@@ -36,11 +33,11 @@ def main():
 
     connection = ConnectGraphDatabase(uri, user, password)
 
-    qString= """
-        MATCH (g:Gene) RETURN g
+    db_query= """
+        placeholder here
         """.format()
     
-    connection.query(qString, db='neo4j')
+    connection.query(db_query, db='neo4j')
 
 if __name__ == "__main__":
     main()
