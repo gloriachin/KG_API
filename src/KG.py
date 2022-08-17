c88901226cef9b54f112095827a1539d6018c5b1
Katie Christensen <katiechristensen@Katies-MBP.systemsbiology.net>
2022-08-17 10:29:04 -0700

    finished project

--- src/KG.py
+++ src/KG.py
@@ -102,25 +102,23 @@ class Query(BaseModel):
 
 
 def parse_query(query:Query):
-    print("In parse_query")
-    
     result = {}
 
     nodes = Dict[str, EdgeParams]
     edges = Dict[str, EdgeParams]
 
-    nodes =  query.message.query_graph.nodes # { "n00": { "categories": [ "biolink:Gene", ], "ids": [ "NCBI:64102" ],},  "n01": { "categories": [ "biolink:Drug"]}}
-    edges =  query.message.query_graph.edges # { "e00": { "object": "n01", "predicates": [ "biolink:targets" ], "subject": "n00"}}
+    nodes =  query.message.query_graph.nodes   # { "n00": { "categories": [ "biolink:Gene", ], "ids": [ "NCBI:64102" ],},  "n01": { "categories": [ "biolink:Drug"]}}
+    edges =  query.message.query_graph.edges   # { "e00": { "object": "n01", "predicates": [ "biolink:targets" ], "subject": "n00"}}
 
     # Handle edges
-    e00: EdgeParams = edges['e00']  # {"object": "n01", "predicates": [ "biolink:targets" ], "attributes":{"biolink:tumor_type":"GBM", "subject": "n00"}
-    e00_predicates = []             # ["biolink:targets"]
-    e00_predicate_type = ''         # "TARGETS"
-    e00_property = []               # ["biolink:tumor_type":"GBM"]
-    e00_property_type = ''          # "TUMOR_TYPE"
-    e00_property_value = ''         # "GBM"
-    subject_node = ''               # "n00"
-    object_node = ''                # "n01"
+    e00: EdgeParams = edges['e00']             # {"object": "n01", "predicates": [ "biolink:targets" ], "attributes":{"biolink:tumor_type":"GBM", "subject": "n00"}
+    e00_predicates = []                        # ["biolink:targets"]
+    e00_predicate_type = ''                    # "TARGETS"
+    e00_property = []                          # ["biolink:tumor_type":"GBM"]
+    e00_property_type = ''                     # "TUMOR_TYPE"
+    e00_property_value = ''                    # "GBM"
+    subject_node = ''                          # "n00"
+    object_node = ''                           # "n01"
 
     e00_predicates = e00.predicates 
     e00_predicate_type = e00_predicates[0].split(':')[1].upper()
@@ -128,18 +126,18 @@ def parse_query(query:Query):
     subject_node = e00.subject
     object_node = e00.object
 
-    if e00.attributes is not None: # this probably isn't correct like might have error splitting list but check this later. 
+    if e00.attributes is not None: #might get an error on splitting a list but lets check that later
         e00_property = e00.attributes 
         e00_property_type = e00_property.split(':')[0].split(':')[1].upper()
         e00_property_value = e00_property.split(':')[1] #.upper()?
 
     # Handle node n00
     subject: NodeParams = nodes[subject_node]  # {"categories": [ "biolink:Gene", ], "ids": [ "NCBI:64102" ]}
-    subject_categories = []             # ["biolink:Gene"]
-    subject_category_type = ''          # "Gene"
-    subject_ids = []                    # ["NCBI:64102"]
-    subject_property_type = ''          # "NCBI"
-    subject_property_value = ''         # "64102"
+    subject_categories = []                    # ["biolink:Gene"]
+    subject_category_type = ''                 # "Gene"
+    subject_ids = []                           # ["NCBI:64102"]
+    subject_property_type = ''                 # "NCBI"
+    subject_property_value = ''                # "64102"
 
     subject_categories = subject.categories 
     subject_category_type = subject_categories[0].split(':')[1] 
@@ -154,14 +152,16 @@ def parse_query(query:Query):
     if (subject_property_type != 'Symbol') & (subject_property_type != 'Name'):
         subject_property_type = subject_property_type + "_ID"
         subject_property_value = int(subject_property_value)
+    else:
+        subject_property_value = subject_property_value.upper()
 
     # Handle node n01
-    object: NodeParams = nodes[object_node]  # {"categories": [ "biolink:Drug"]}
-    object_categories = []             # ["biolink:Drug"]
-    object_category_type = ''          # "Drug"
-    object_ids = []                    # ["Name:Afatinib"]
-    object_property_type = ''          # "Name"
-    object_property_value = ''         # "AFATINIB"
+    object: NodeParams = nodes[object_node]    # {"categories": [ "biolink:Drug"]}
+    object_categories = []                     # ["biolink:Drug"]
+    object_category_type = ''                  # "Drug"
+    object_ids = []                            # ["Name:Afatinib"]
+    object_property_type = ''                  # "Name"
+    object_property_value = ''                 # "AFATINIB"
     
     object_categories = object.categories 
     object_category_type = object_categories[0].split(':')[1] 
@@ -176,6 +176,8 @@ def parse_query(query:Query):
     if (object_property_type != 'Symbol') & (object_property_type != 'Name'):
         object_property_type = object_property_type + "_ID"
         object_property_value = int(object_property_value)
+    else:
+        object_property_value = object_property_value.upper()
 
     string1 = 'n00:' + subject_category_type
     string2 = 'e00:' + e00_predicate_type
@@ -191,8 +193,8 @@ def parse_query(query:Query):
     # WHERE n00.n00_property_type=n00_property_value AND n01.n01_property_type=n01_property_value
     # RETURN DISTINCT n00, e00, n01
 
-    # MATCH (n00:Gene)-[e00:TARGETS]-(n01:Drug)
-    # WHERE n00.NCBI_ID=64102 AND n01.Name="AFATINIB"
+    # MATCH (n00:Drug)-[e00:TARGETS]-(n01:Gene)
+    # WHERE n00.Name="PACLITAXEL" AND n01.Symbol="BCL2"
     # RETURN DISTINCT n00, e00, n01
 
     result= {"string1": string1, "string2": string2, "string3": string3, "string4": string4,"string5": string5}
@@ -200,39 +202,21 @@ def parse_query(query:Query):
 
 
 def query_KG(query,db,string1,string2,string3,string4,string5):
-    print("In query_KG")
-
     if (string4 != 'n00.=') & (string5 != 'n01.='):
-        query = '''
-            MATCH ({string1})-[{string2}]-({string3})
-            WHERE {string4} AND {string5}
-            RETURN DISTINCT n00, e00, n01 LIMIT 1
-            '''.format(string1=string1,string2=string2,string3=string3,string4=string4,string5=string5)
+        query = ''' MATCH ({string1})-[{string2}]-({string3}) WHERE {string4} AND {string5} RETURN DISTINCT n00, e00, n01'''.format(string1=string1,string2=string2,string3=string3,string4=string4,string5=string5) # REMINDER: REMOVE THE LIMIT !!
+    
     elif (string4 == 'n00.='):
-        query = '''
-            MATCH ({string1})-[{string2}]-({string3})
-            WHERE {string5}
-            RETURN DISTINCT n00, e00, n01
-            '''.format(string1=string1,string2=string2,string3=string3,string5=string5)
+        query = '''MATCH ({string1})-[{string2}]-({string3}) WHERE {string5} RETURN DISTINCT n00, e00, n01'''.format(string1=string1,string2=string2,string3=string3,string5=string5)
+    
     elif (string5 == 'n01.='):
-        query = '''
-            MATCH ({string1})-[{string2}]-({string3})
-            WHERE {string4}
-            RETURN DISTINCT n00, e00, n01
-            '''.format(string1=string1,string2=string2,string3=string3,string4=string4)
+        query = '''MATCH ({string1})-[{string2}]-({string3}) WHERE {string4} RETURN DISTINCT n00, e00, n01'''.format(string1=string1,string2=string2,string3=string3,string4=string4)
+    
     else:
         query = ''''''
 
-    print(query)
     result = db.query(query, db='neo4j')
 
     response_message={}
-    
-    #response["message"] = {"query_graph":query.message.query_graph,
-    #
-    #                            "knowledge_graph":{"edges":{},
-    #                                                "nodes":{}}, 
-    #                            "results":{}}
 
     response_message["query_graph"] = query
     response_message["results"] = []
@@ -241,41 +225,52 @@ def query_KG(query,db,string1,string2,string3,string4,string5):
     response_message["knowledge_graph"]["nodes"] =  {}
 
     for word in result:
-        print(word[0])
-        response_message['knowledge_graph']['edges']["n00-n01"] = {
-                                                                "Subject": word[1],
-                                                                "Object": word[1],
-                                                                "Predicate": word[0],
-                                                                "Edge_attributes": {
-                                                                "Edge_attribute_publications": word[1],
-                                                                "Edge_attribute_knowledge_source": word[1],
-                                                                "Edge_attribute_provided_by": word[1],
-                                                                "Edge_attribute_FDA_approval_status": word[1] 
+        w = dict(word)
+        n0 = dict(w.get("n00"))
+        e0 = dict(w.get("e00"))
+        n1 = dict(w.get("n01"))
+
+        response_message['knowledge_graph']['nodes']["n00"] = {
+                                                            "Subject_Name": n0.get("Name"),
+                                                            "Subject_Symbol": n0.get("Symbol"),
+                                                            "Subject_Category": n0.get("Category"),
+                                                            "Subject_attributes": 
+                                                                {
+                                                                "Subject_NCBI_ID": n0.get("NCBI_ID"),
+                                                                "Subject_MONDO_ID": n0.get("MONDO_ID"),
+                                                                "Subject_Chembl_ID": n0.get("Chembl_ID"),
+                                                                "Subject_Pubchem_ID": n0.get("Pubchem_ID"),
+                                                                "Subject_Synonym": n0.get("Synonym"),
+                                                                "Subject_Prefixes": n0.get("Prefixes")
                                                                 }
                                                             }
 
-        response_message['knowledge_graph']['nodes']["n00"] = {
-                                                            "Subject_Category": word[0],
-                                                            "Subject_Name": word[0],
-                                                            "Subject_Symbol": word[0],
-                                                            "Subject_NCBI_ID": word[0],
-                                                            "Subject_MONDO_ID": word[0],
-                                                            "Subject_Chembl_ID": word[0],
-                                                            "Subject_Pubchem_ID": word[0],
-                                                            "Subject_Synonym": word[0],
-                                                            "Subject_Prefixes": word[0]
+        response_message['knowledge_graph']['edges']["n00-n01"] = {
+                                                            #"Subject": e0.something,
+                                                            #"Object": e0.something,
+                                                            #"Predicate": e0.something,
+                                                            "Edge_attributes": 
+                                                                {
+                                                                "Edge_attribute_publications": e0.get("Publications"),
+                                                                "Edge_attribute_knowledge_source": e0.get("Knowledge_Source"),
+                                                                "Edge_attribute_provided_by": e0.get("Provided_by"),
+                                                                "Edge_attribute_FDA_approval_status": e0.get("FDA_approval_status")
+                                                                }
                                                             }
 
         response_message['knowledge_graph']['nodes']["n01"] = {
-                                                            "Object_Chembl_ID": word[2],
-                                                            "Object_NCBI_ID": word[2],  
-                                                            "Object_Name": word[2],     
-                                                            "Object_Category": word[2], 
-                                                            "Object_Synonym": word[2],
-                                                            "Object_Pubchem_ID": word[2],
-                                                            "Object_MONDO_ID": word[2],
-                                                            "Object_Prefixes": word[2], 
-                                                            "Object_Symbol": word[2] 
+                                                            "Object_Name": n1.get("Name"),
+                                                            "Object_Symbol": n1.get("Symbol"),
+                                                            "Object_Category": n1.get("Category"),
+                                                            "Object_attributes": 
+                                                                {
+                                                                "Object_NCBI_ID": n1.get("NCBI_ID"),
+                                                                "Object_MONDO_ID": n1.get("MONDO_ID"),
+                                                                "Object_Chembl_ID": n1.get("Chembl_ID"),
+                                                                "Object_Pubchem_ID": n1.get("Pubchem_ID"),
+                                                                "Object_Synonym": n1.get("Synonym"),
+                                                                "Object_Prefixes": n1.get("Prefixes")
+                                                                }
                                                             }
 
     response = {}
